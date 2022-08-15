f4e056482c93beb4c31148136b4426237bc5b14a
ranadtalalljeeran <ranadtalalaljiran@gmail.com>
2022-08-15 13:35:51 +0300

    .

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let earlyStudent = ["aisha", "besma", "mariam"]
+
+let onTimeStudent = ["nour", "sala", "rayan"]
+
+var lateStudent = ["sana", "laya", "shaikha"]
+
+
+earlyStudent.count
+earlyStudent.randomElement()
+
+lateStudent.append("ranad")
+print(lateStudent)
+
+lateStudent.count
+lateStudent.randomElement()
+lateStudent.remove(at:0)
+print(lateStudent)
+
